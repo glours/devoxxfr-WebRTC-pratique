1b4bcd40916b0d89b7bcc9d7af50f937f4606380
Guillaume Lours <g.lours@agileit.fr>
2016-04-10 18:46:09 +0200

    feat (step-1) : Mirroring local video stream
    Add video element in index.html and javascript to start and stream the local video stream to the video element

--- app/js/webrtc-natif.js
+++ app/js/webrtc-natif.js
@@ -1,4 +1,36 @@
+var localStream;
 function isUserMediaSupported() {
     return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+function userMedia() {
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+    return navigator.getUserMedia;
+}
+
+function errorCallback(event) {
+    console.log(event);
+}
+
+function mirrorVideo(videoContent, useVideo, useAudio) {
+    navigator.getUserMedia({video : useVideo, audio : useAudio}, function(stream) {
+        localStream = stream;
+        videoContent.src = window.URL.createObjectURL(stream);;
+    }, errorCallback);
+}
+
+/* not yet supported by chrome by default */
+function mirrorFutureVersion(videoContent, useVideo, userAudio) {
+    navigator.mediaDevices.getUserMedia({video : useVideo, audio : useAudio})
+        .then(function(stream) {
+            localStream = stream;
+            videoContent.srcObject = localStream;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 }
--- app/js/webrtc-page.js
+++ app/js/webrtc-page.js
@@ -1,12 +1,26 @@
 function displayWebRTCSupport(checkElementId, contentElementId) {
+    var contentElement = $("#" + contentElementId);
     if($('#' + checkElementId).is(':checked')) {
         var isSupported = 'Your browser support getUserMedia !';
         if (!isUserMediaSupported()) {
             isSupported = 'getUserMedia() is not supported in your browser';
         }
-        $("#" + contentElementId).html(isSupported);
-        $('#' + contentElementId).show();
+        contentElement.html(isSupported);
+        contentElement.show();
     } else {
-        $('#' + contentElementId).hide();
+        contentElement.hide();
+    }
+}
+
+function displayMirrorVideo(checkElementId, videoElementId) {
+    if($('#' + checkElementId).is(':checked')) {
+        if (userMedia()) {
+            var mirror = $("#" + videoElementId)[0];
+            mirrorVideo(mirror, true, false);
+        } else {
+            console.log("No userMedia :'-(");
+        }
+    } else {
+        $('#' + videoElementId).hide();
     }
 }
