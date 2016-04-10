eb604516f1330004a2155c19c6c225a1cc5b112c
Guillaume Lours <g.lours@agileit.fr>
2016-04-10 20:47:01 +0200

    feat (video constraints) : add Video Constraints
    Add video constaints if video accepted

--- app/js/webrtc-natif.js
+++ app/js/webrtc-natif.js
@@ -1,4 +1,26 @@
 var localStream;
+
+function hdConstraints(useVideo, useAudio) {
+    var hdConstraints = {};
+    if(useVideo) {
+        hdConstraints.video = {
+            mandatory: {
+                minWidth: 640,
+                minHeight: 480,
+                maxWidth: 1280,
+                maxHeight: 720
+            },
+            optional: [
+                {frameRate: 60},
+                {aspectRatio: true},
+                {facingMode: "user"}
+            ]
+        };
+    }
+    hdConstraints.audio = useAudio;
+    return hdConstraints;
+}
+
 function isUserMediaSupported() {
     return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia);
@@ -17,7 +39,7 @@ function errorCallback(event) {
 }
 
 function mirrorVideo(videoContent, useVideo, useAudio) {
-    navigator.getUserMedia({video : useVideo, audio : useAudio}, function(stream) {
+    navigator.getUserMedia(hdConstraints(useVideo, useAudio), function(stream) {
         localStream = stream;
         videoContent.src = window.URL.createObjectURL(stream);;
     }, errorCallback);
